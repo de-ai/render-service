8d8e610f49494d7d1c22bbf44ba99c8b6c90e0dd
mtholcombe <matt@designengine.ai>
2019-04-26 15:49:36 -0400

    base commit

--- index.js
+++ index.js
@@ -10,43 +10,51 @@ app.use('/media', express.static(path.resolve(__dirname, 'media')));
 
 // hide powered by express
 app.disable('x-powered-by');
+
 // start the server
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
 
 // our apps data model
 const data = require('./assets/data.json');
 
 let initialState = {
-  isFetching: false,
-  apps: data
-}
+  isFetching : false,
+  apps       : data
+};
 
 //SSR function import
 const ssr = require('./views/server');
 
 // server rendered home page
-app.get('/', (req, res) => {
-  const { preloadedState, content}  = ssr(initialState)
-  const response = template("Server Rendered Page", preloadedState, content)
-  res.setHeader('Cache-Control', 'assets, max-age=604800')
+app.get('/', (req, res)=> {
+  const { preloadedState, content } = ssr(initialState);
+  const response = template('Server Rendered Page', preloadedState, content);
+  res.setHeader('Cache-Control', 'assets, max-age=604800');
   res.send(response);
 });
 
 // Pure client side rendered page
-app.get('/client', (req, res) => {
-  let response = template('Client Side Rendered page')
-  res.setHeader('Cache-Control', 'assets, max-age=604800')
+app.get('/client', (req, res)=> {
+  let response = template('Client Side Rendered page');
+  res.setHeader('Cache-Control', 'assets, max-age=604800');
   res.send(response);
 });
 
+// contents of file
+app.get('/googlefdcf5eff7ee69de9.html', (req, res) => {
+  res.send('google-site-verification: googlefdcf5eff7ee69de9.html');
+});
+
 // tiny trick to stop server during local development
+app.get('/exit', (req, res)=> {
+  if (process.env.PORT) {
+    res.send('Sorry, the server denies your request');
 
-  app.get('/exit', (req, res) => {
-    if(process.env.PORT) {
-      res.send("Sorry, the server denies your request")
-    } else {
-      res.send("shutting down")
-      process.exit(0)
-    }
+  } else {
+    res.send('Shutting down…');
+    process.exit(0);
+  }
+});
 
-  });
+
+//-- new Promise((_)=> null);
--- src/template.js
+++ src/template.js
@@ -1,15 +1,18 @@
 // html skeleton provider
-function template(title, initialState = {}, content = ""){
+function template(title, initialState={}, content=''){
+//=-   console.log('[::]', 'template.js -/> ()', title, initialState, content);
+
   let scripts = ''; // Dynamically ship scripts based on render type
-  if(content){
+  if (content) {
     scripts = ` <script>
                    window.__STATE__ = ${JSON.stringify(initialState)}
                 </script>
                 <script src="assets/client.js"></script>
-                `
+              `;
   } else {
-    scripts = ` <script src="assets/bundle.js"> </script> `
+    scripts = ` <script src="assets/bundle.js"> </script> `;
   }
+
   let page = `<!DOCTYPE html>
               <html lang="en">
               <head>
@@ -28,7 +31,7 @@ function template(title, initialState = {}, content = ""){
               </body>
               `;
 
-  return page;
+  return (page);
 }
 
 module.exports = template;
